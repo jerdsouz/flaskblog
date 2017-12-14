f93f063ec6510678cb8b07c75cbcb22638595efd
jerdsouz <jerome.c.dsouza@gmail.com>
2017-12-14 16:10:02 -0500

    -

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,15 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    user = {'nickname': 'Jerome'}
-    return render_template('index.html', title="Hello", user=user)
+    user = {'username': 'Jerome'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in portland'
+         },
+        {
+            'author': {'username': 'John'},
+            'body': 'The avengers moview was so cool'
+        }
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
